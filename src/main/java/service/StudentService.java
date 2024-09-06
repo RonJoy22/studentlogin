28ae4b73624586ce0ef816a85bae5867146fba6b
ronald.joyner-brown <r.brownjoyner@gmail.com>
2024-09-06 11:53:10 -0400

    Initial commit

---
+++ src/main/java/service/StudentService.java
@@ -0,0 +1,12 @@
+package service;
+
+import model.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    void saveStudent(StudentDTO studentDto);
+    Student findStudentByEmail(String email);
+    List<StudentDTO> findAllStudents();
+}
+
